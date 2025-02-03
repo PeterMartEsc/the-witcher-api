343e79dd5b69fb3b20d13edf37250a45a7920220
PeterMartEsc <petermartesc@gmail.com>
2025-02-03 19:54:41 +0000

    feat(security jwt): se empieza a trabajar en la seguridad de jwt filter y token con la parte de Api REST

--- code/src/main/java/petermartesc/springboot/repository/UserRepository.java
+++ code/src/main/java/petermartesc/springboot/repository/UserRepository.java
@@ -1,10 +1,20 @@
 package petermartesc.springboot.repository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import petermartesc.springboot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
+
+    @Query(
+            value="SELECT * FROM usuarios WHERE nombre = :nombre",
+            nativeQuery = true
+    ) //Native Query
+    Optional<User> findByNombre(@Param("nombre") String nombre);
 
 }
---
+++ code/src/main/java/petermartesc/springboot/security/AuthService.java
@@ -0,0 +1,75 @@
+package petermartesc.springboot.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import petermartesc.springboot.model.Role;
+import petermartesc.springboot.model.User;
+import petermartesc.springboot.repository.RoleRepository;
+import petermartesc.springboot.repository.UserRepository;
+
+import java.util.Date;
+import java.util.UUID;
+
+
+@Service
+public class AuthService {
+	
+	
+    @Autowired
+    private UserRepository userRepository;
+
+	@Autowired
+	private RoleRepository roleRepository;
+	
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+	/*@Autowired
+	private MailService mailService;*/
+
+    
+	public String register(String username, String password, String email) {
+		User user = new User();
+		user.setName(username);
+		user.setPassword(passwordEncoder.encode(password));
+		Role roleUser = roleRepository.findById(2).orElse(null);
+		if(roleUser == null){
+			return null;
+		}
+		user.setRole(roleUser);
+
+		User saved = userRepository.save(user);
+		
+		if( saved != null) {
+			/*String senders[] = {"apps.akameterindustries@gmail.com", email};
+			mailService.send(senders, "user creado: "+user.getNombre(),
+					"http://localhost:8080/api/v1/confirmacion/?correo="+user.getCorreo()+"&token="+tokenVerifCorreo);*/
+			String generatedToken = jwtService.generateToken(user.getName(), user.getRole().getRoleName());
+			return generatedToken;
+		}else {
+			return null;
+		}
+	}    
+    
+
+
+	public String authenticate(String username, String password)  {
+		String generatedToken = null;
+		User user = userRepository.findByNombre(username).orElse(null);
+		//System.out.println(user.getNombre());
+		if (user != null) {
+			//System.out.println("no es nulo");
+			if (passwordEncoder.matches(password, user.getPassword())) {
+				//System.out.println("tiene contraseña y coincide con la de la bbdd");
+				generatedToken = jwtService.generateToken(user.getName(), user.getRole().getRoleName());
+			}
+		}
+
+		return generatedToken;
+	}
+}
+
